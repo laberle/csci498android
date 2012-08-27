9585d6591b80eaa505a04f2eedbea889fa639177
Lauren Aberle <laberle@mines.edu>
2012-08-27 00:47:15 -0600

    Added button that displays current time, got rid of boring HelloWorld text.

--- HelloAndroid/src/csci498/hello/HelloAndroidActivity.java
+++ HelloAndroid/src/csci498/hello/HelloAndroidActivity.java
@@ -1,20 +1,32 @@
 package csci498.hello;
 
+import java.util.Date;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class HelloAndroidActivity extends Activity {
+public class HelloAndroidActivity extends Activity implements View.OnClickListener {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello_android);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_hello_android, menu);
-        return true;
-    }
+	Button btn;
+	
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		btn = new Button(this);
+		btn.setOnClickListener(this);
+		updateTime();
+		setContentView(btn);
+	}
+	
+	public void onClick(View view) {
+		updateTime();
+	}
+	
+	private void updateTime() {
+		btn.setText(new Date().toString());
+	}
+	
 }
