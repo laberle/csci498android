6a58f8e05e82d50c892a643e54d90bc70bb41960
Lauren Aberle <laberle@mines.edu>
2012-08-27 22:47:52 -0600

    Added basic Restaurant functionality.

--- LunchList/src/csci498/laberle/lunchlist/LunchListActivity.java
+++ LunchList/src/csci498/laberle/lunchlist/LunchListActivity.java
@@ -10,10 +10,24 @@ import android.widget.EditText;
 
 public class LunchListActivity extends Activity {
 
+	Restaurant restaurant = new Restaurant();
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lunch_list);
+
+		Button save = (Button) findViewById(R.id.save);
+		save.setOnClickListener(onSave);
 	}
+
+	private View.OnClickListener onSave = new OnClickListener() {
+		public void onClick(View v) {
+			EditText name = (EditText) findViewById(R.id.name);
+			EditText address = (EditText) findViewById(R.id.addr);
+			restaurant.setName(name.getText().toString());
+			restaurant.setAddress(address.getText().toString());
+		}
+	};
 
 }
