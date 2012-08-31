9865e4695e05c19416517d2264889425fae31b14
Lauren Aberle <laberle@mines.edu>
2012-08-30 21:06:34 -0600

    Finished basic Tutorial 3 steps (no EC)

--- LunchList/src/csci498/laberle/lunchlist/LunchListActivity.java
+++ LunchList/src/csci498/laberle/lunchlist/LunchListActivity.java
@@ -2,11 +2,11 @@ package csci498.laberle.lunchlist;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class LunchListActivity extends Activity {
 
@@ -27,6 +27,19 @@ public class LunchListActivity extends Activity {
 			EditText address = (EditText) findViewById(R.id.addr);
 			restaurant.setName(name.getText().toString());
 			restaurant.setAddress(address.getText().toString());
+
+			RadioGroup types = (RadioGroup) findViewById(R.id.types);
+			switch (types.getCheckedRadioButtonId()) {
+			case R.id.sit_down:
+				restaurant.setType("sit_down");
+				break;
+			case R.id.take_out:
+				restaurant.setType("take_out");
+				break;
+			case R.id.delivery:
+				restaurant.setType("delivery");
+				break;
+			}
 		}
 	};
 
--- LunchList/src/csci498/laberle/lunchlist/Restaurant.java
+++ LunchList/src/csci498/laberle/lunchlist/Restaurant.java
@@ -2,20 +2,27 @@ package csci498.laberle.lunchlist;
 
 public class Restaurant {
 
-	private String name = "";
 	private String address = "";
+	private String name = "";
+	private String type = "";
 
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getAddress() {
-		return address;
+	public String getType() {
+		return type;
 	}
-	public void setAddress(String address) {
-		this.address = address;
+	public void setType(String type) {
+		this.type = type;
 	}
 	
 }
