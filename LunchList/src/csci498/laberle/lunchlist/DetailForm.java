644b4d5425d2e4bee9ba5dabe1ac1a29ea59c879
Lauren Aberle <laberle@mines.edu>
2012-09-23 11:33:39 -0600

    Added stub activity for Detail form.

---
+++ LunchList/src/csci498/laberle/lunchlist/DetailForm.java
@@ -0,0 +1,12 @@
+package csci498.laberle.lunchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailForm extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+}
