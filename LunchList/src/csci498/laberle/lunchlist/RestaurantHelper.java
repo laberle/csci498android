c29f0421e16e1a828626d0c5be9ae7ffcdf8cbe8
Lauren Aberle <laberle@mines.edu>
2012-09-29 11:06:28 -0600

    Added feed field to restaurants table.

--- LunchList/src/csci498/laberle/lunchlist/RestaurantHelper.java
+++ LunchList/src/csci498/laberle/lunchlist/RestaurantHelper.java
@@ -9,7 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class RestaurantHelper extends SQLiteOpenHelper {
 
 	private static final String DATABASE_NAME = "lunchlist.db";
-	private static final int SCHEMA_VERSION = 1;
+	private static final int SCHEMA_VERSION = 2;
 
 	public RestaurantHelper(Context context) {
 		super(context, DATABASE_NAME, null, SCHEMA_VERSION);
@@ -23,7 +23,8 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 			"address TEXT," +
 			"restaurant_type_id INTEGER," +
 			"notes TEXT," +
-			"date TEXT);");
+			"date TEXT," +
+			"feed TEXT);");
 		db.execSQL("CREATE TABLE restaurant_types (" +
 			"_id INTEGER PRIMARY KEY AUTOINCREMENT," +
 			"name TEXT);");
@@ -44,11 +45,11 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// Intentionally left blank - need 2nd schema before this will be called
+		db.execSQL("ALTER TABLE restaurants ADD COLUMN feed TEXT");
 	}
 
 	public void insert(String name, String address,
-		int typeId, String notes, String date) {
+		int typeId, String notes, String date, String feed) {
 
 		ContentValues cv = new ContentValues();
 
@@ -57,6 +58,7 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 		cv.put("restaurant_type_id", typeId);
 		cv.put("notes", notes);
 		cv.put("date", date);
+		cv.put("feed", feed);
 
 		if (cv.size() > 0) {
 			getWritableDatabase().insert("restaurants", null, cv);
@@ -64,7 +66,7 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 	}
 	
 	public void update(String id, String name, String address, 
-		int typeId, String notes, String date) {
+		int typeId, String notes, String date, String feed) {
 
 		ContentValues cv = new ContentValues();
 		String[] args = {id};
@@ -74,6 +76,7 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 		cv.put("restaurant_type_id", typeId);
 		cv.put("notes", notes);
 		cv.put("date", date);
+		cv.put("feed", feed);
 
 		if (cv.size() > 0) {
 			getWritableDatabase().update("restaurants", cv, "_id=?", args);
@@ -83,7 +86,7 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 	public Cursor getAll(String orderBy) {
 		return getReadableDatabase()
 			.rawQuery("SELECT restaurants._id, restaurants.name as name, address, " +
-					"restaurant_types.name, notes, date " +
+					"restaurant_types.name, notes, date, feed " +
 				"FROM restaurants, restaurant_types " +
 				"WHERE restaurants.restaurant_type_id = restaurant_types._id " +
 				"ORDER BY " + orderBy,
@@ -95,7 +98,7 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 		
 		return getReadableDatabase()
 			.rawQuery("SELECT restaurants._id, restaurants.name, address, " +
-					"restaurant_types.name, notes, date " +
+					"restaurant_types.name, notes, date, feed " +
 				"FROM restaurants, restaurant_types " +
 				"WHERE restaurants.restaurant_type_id = restaurant_types._id " +
 				"AND restaurants._id = ? " +
@@ -117,6 +120,9 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 	}
 	public String getDate(Cursor c) {
 		return c.getString(5);
+	}
+	public String getFeed(Cursor c) {
+		return c.getString(6);
 	}
 
 }
