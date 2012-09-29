4a8bbaf92204ed5843fe49dea58d954e7f3d62ec
Lauren Aberle <laberle@mines.edu>
2012-09-29 10:55:23 -0600

    Override onSaveInstanceState and onRestoreInstanceState to preserve state when DetailForm is rotated.

--- LunchList/src/csci498/laberle/lunchlist/DetailForm.java
+++ LunchList/src/csci498/laberle/lunchlist/DetailForm.java
@@ -30,6 +30,42 @@ public class DetailForm extends Activity {
 
 		initializeMembers();
 	}
+	
+	
+	@Override
+	public void onSaveInstanceState(Bundle state) {
+		super.onSaveInstanceState(state);
+		
+		String dateString =  ((Integer) datePicker.getMonth()).toString()
+			+ " " + ((Integer) datePicker.getDayOfMonth()).toString()
+			+ " " + ((Integer) datePicker.getYear()).toString();
+		
+		state.putString("name", name.getText().toString());
+		state.putString("address", address.getText().toString());
+		state.putString("notes", notes.getText().toString());
+		state.putInt("type", types.getCheckedRadioButtonId());
+		state.putString("date", dateString);
+	}
+	
+	@Override
+	public void onRestoreInstanceState(Bundle state) {
+		super.onRestoreInstanceState(state);
+		
+		name.setText(state.getString("name"));
+		address.setText(state.getString("address"));
+		notes.setText(state.getString("notes"));
+		types.check(state.getInt("type"));
+		
+		String date = state.getString("date");
+		GregorianCalendar cal = (GregorianCalendar) GregorianCalendar.getInstance();
+		String[] dateString = date.split(" ");
+		cal.set(Integer.parseInt(dateString[2]), 
+			Integer.parseInt(dateString[0]),
+			Integer.parseInt(dateString[1]));
+		datePicker.init(cal.get(Calendar.YEAR), 
+			cal.get(Calendar.MONTH), 
+			cal.get(Calendar.DATE), null);
+	}
 
 	@Override
 	public void onDestroy() {
