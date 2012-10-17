1ccfe5b1a0bc6abfef951b0c3ded21bf09314243
Lauren Aberle <laberle@mines.edu>
2012-10-16 20:45:32 -0600

    Added IntentService (FeedService) and basic implementation.

--- LunchList/src/csci498/laberle/lunchlist/FeedActivity.java
+++ LunchList/src/csci498/laberle/lunchlist/FeedActivity.java
@@ -1,20 +1,64 @@
 package csci498.laberle.lunchlist;
 
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSItem;
 import org.mcsoxford.rss.RSSReader;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
-
+@SuppressWarnings("deprecation")
 public class FeedActivity extends ListActivity {
+	
+	private InstanceState state;
+	public static final String FEED_URL = "csci498.laberle.lunchlist.FEED_URL";
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		adjustState();
+	}
 
+	private void adjustState() {
+		state = (InstanceState) getLastNonConfigurationInstance();
+		
+		if (state == null) {
+			state = new InstanceState();
+			state.task = new FeedTask(this);
+			state.task.execute(getIntent().getStringExtra(FEED_URL));
+		}
+		else {
+			if (state.task != null) {
+				state.task.attach(this);
+			}
+			if (state.feed != null) {
+				setFeed(state.feed);
+			}
+		}
+	}
+	
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		if (state.task != null) {
+			state.task.detach();
+		}
+		return state;
+	}
+	
+	private void setFeed(RSSFeed feed) {
+		state.feed = feed;
+		setListAdapter(new FeedAdapter(feed));
+	}
+	
 	private void goBlooey(Throwable t) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle("Exception!")
@@ -62,5 +106,44 @@ public class FeedActivity extends ListActivity {
 				activity.goBlooey(e);
 			}
 		}
+	}
+	
+	private class FeedAdapter extends BaseAdapter {
+		RSSFeed feed;
+		
+		FeedAdapter(RSSFeed feed) {
+			super();
+			this.feed = feed;
+		}
+		
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View row = convertView;
+			
+			if (row == null) {
+				LayoutInflater inflater = getLayoutInflater();
+				row = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+			}
+			
+			RSSItem item = (RSSItem) getItem(position);
+			((TextView) row).setText(item.getTitle());
+			return row;
+		}
+		
+		public int getCount() {
+			return feed.getItems().size();
+		}
+		
+		public Object getItem(int position) {
+			return feed.getItems().get(position);
+		}
+		
+		public long getItemId(int position) {
+			return position;
+		}
+	}
+	
+	private static class InstanceState {
+		RSSFeed feed;
+		FeedTask task;
 	}
 }
