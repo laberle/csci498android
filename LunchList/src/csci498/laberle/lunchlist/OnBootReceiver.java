5ed0da19a5ca0671c139cb27b50edf6b80bab89d
Lauren Aberle <laberle@mines.edu>
2012-10-29 10:56:37 -0600

    Added onBootReceiver and added permissions and receiver to Manifest.

---
+++ LunchList/src/csci498/laberle/lunchlist/OnBootReceiver.java
@@ -0,0 +1,16 @@
+package csci498.laberle.lunchlist;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class OnBootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
