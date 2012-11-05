6a315f5c16b50882decfdba714bd6aed0ae135a4
Lauren Aberle <laberle@mines.edu>
2012-11-05 11:29:05 -0700

    Integrated WidgetService to update widget with random restaurant name. Done with APT 21.

--- LunchList/src/csci498/laberle/lunchlist/AppWidget.java
+++ LunchList/src/csci498/laberle/lunchlist/AppWidget.java
@@ -1,8 +1,15 @@
 package csci498.laberle.lunchlist;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 
 
 public class AppWidget extends AppWidgetProvider {
 
+	@Override
+	public void onUpdate(Context context, AppWidgetManager manager, int[] appwidgetIds) {
+		context.startService(new Intent(context, WidgetService.class));
+	}
 }
---
+++ LunchList/src/csci498/laberle/lunchlist/WidgetService.java
@@ -0,0 +1,67 @@
+package csci498.laberle.lunchlist;
+
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.database.Cursor;
+import android.widget.RemoteViews;
+
+
+public class WidgetService extends IntentService {
+
+	public WidgetService() {
+		super("WidgetService");
+	}
+
+	@Override
+	public void onHandleIntent(Intent intent) {
+		ComponentName componentName = new ComponentName(this, AppWidget.class);
+		RemoteViews updateViews = new RemoteViews("csci498.laberle.lunchlist", R.layout.widget);
+		RestaurantHelper helper = new RestaurantHelper(this);
+		AppWidgetManager manager = AppWidgetManager.getInstance(this);
+
+		try {
+			Cursor c = helper
+				.getReadableDatabase()
+				.rawQuery("SELECT COUNT(*) FROM restaurants", null);
+			c.moveToFirst();
+			int count = c.getInt(0);
+			c.close();
+
+			if (count > 0) {
+				int offset = (int) (count * Math.random());
+				String args[] = {String.valueOf(offset)};
+
+				c = helper
+					.getReadableDatabase()
+					.rawQuery("SELECT _ID, name FROM restaurants LIMIT 1 OFFSET ?", args);
+				c.moveToFirst();
+				
+				updateViews.setTextViewText(R.id.name, c.getString(1));
+				
+				Intent i = new Intent(this, DetailForm.class);
+				i.putExtra(LunchList.ID_EXTRA, c.getString(0));
+				PendingIntent pendingIntent = PendingIntent.getActivity(this,
+					0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+				
+				updateViews.setOnClickPendingIntent(R.id.name, pendingIntent);
+				
+				c.close();
+			}
+			else {
+				updateViews.setTextViewText(R.id.title, this.getString(R.string.empty));
+			}
+		}
+		finally {
+			helper.close();
+		}
+		
+		Intent i = new Intent(this, WidgetService.class);
+		PendingIntent pendingIntent = PendingIntent.getService(this, 0, i, 0);
+		
+		updateViews.setOnClickPendingIntent(R.id.next, pendingIntent);
+		manager.updateAppWidget(componentName, updateViews);
+	}
+}
